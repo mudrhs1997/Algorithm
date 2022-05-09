6e40087c9912afd4ffce13e08022207d29898144
mudrhs1997 <mudrhs1997@daum.net>
2022-05-09 20:17:18 +0900

    Update [삼성]연산자 끼워넣기_14888.swift

--- BOJ/Swift/백트래킹/[삼성]연산자 끼워넣기_14888.swift
+++ BOJ/Swift/백트래킹/[삼성]연산자 끼워넣기_14888.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+// bfs로 다시 풀어보자
+
 let N = Int(readLine()!)!
 let seq = readLine()!.components(separatedBy: " ").map{Int($0)!}
 let numOfExpression = readLine()!.components(separatedBy: " ").map{Int($0)!}
@@ -64,13 +66,8 @@ func cal(index: Int, r: Int) {
                 result = result * seq[index]
                 resultArr.append(result)
             } else if expression[ex].0 == "/" && expression[ex].1 == false {
-                if result < 0 {
-                    result = -(Int(result.magnitude) / seq[index])
-                    resultArr.append(result)
-                } else {
                     result = result / seq[index]
                     resultArr.append(result)
-                }
             }
             
         } else {
@@ -93,19 +90,11 @@ func cal(index: Int, r: Int) {
                 expression[ex].1 = false
                 result = r
             } else if expression[ex].0 == "/" && expression[ex].1 == false {
-                if result < 0 {
-                    result = -(Int(result.magnitude) / seq[index])
-                    expression[ex].1 = true
-                    cal(index: index+1, r: result)
-                    expression[ex].1 = false
-                    result = r
-                } else {
                     result = result / seq[index]
                     expression[ex].1 = true
                     cal(index: index+1, r: result)
                     expression[ex].1 = false
                     result = r
-                }
             }
             
         }
