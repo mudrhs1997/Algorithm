bf7629ea19f900445ded421d7205aa4c223ba2d4
mudrhs1997 <mudrhs1997@daum.net>
2022-09-23 23:49:31 +0900

    Update ATM_11399.py

--- BOJ/Python/ATM_11399.py
+++ BOJ/Python/ATM_11399.py
@@ -1,1 +1,12 @@
-ATMS
+import  sys
+
+n = int(sys.stdin.readline())
+line = sorted(list(map(int, sys.stdin.readline().split())))
+memo = 0
+sum = 0
+
+for i in line:
+    memo += i
+    sum += memo
+
+print(sum)
